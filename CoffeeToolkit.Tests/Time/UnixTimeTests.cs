19f2faa1e9503c1c1dc7d7f9a80fb920be7c2d9c
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-11-21 13:50:35 +0100

    VS Project Files + Time.UnixTime
    - Including unittests

---
+++ CoffeeToolkit.Tests/Time/UnixTimeTests.cs
@@ -0,0 +1,48 @@
+﻿using CoffeeToolkit.Time;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeToolkit.Tests.Time
+{
+    class UnixTimeTests
+    {
+        [Test]
+        [TestCase("2020-01-01 0:00:00", 1577836800)]
+        [TestCase("2020-01-01 0:00:00.999", 1577836800, Description = "Expect floor")]
+        [TestCase("1969-01-01 0:00:00", -31536000, Description = "Expect negative")]
+        public void FromDateTime_TestResults(string dtStr, int expected)
+        {
+            DateTime input = DateTime.Parse(dtStr);
+            int actual = UnixTime.FromDateTime(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test(Description = "Expect OutOfRangeException for compatability")]
+        [TestCase("2040-01-01 0:00:00", Description = "Too high")]
+        [TestCase("1900-01-01 0:00:00", Description = "Too low")]
+        public void FromDateTime_OutOfRange_ExpectException(string dtStr)
+        {
+            DateTime input = DateTime.Parse(dtStr);
+            try
+            {
+                int result = UnixTime.FromDateTime(input);
+                Assert.Fail("Result type should be int to maintain compatability. Create another method.");
+            }
+            catch (ArgumentOutOfRangeException) { Assert.Pass(); };
+        }
+
+        [Test]
+        [TestCase("2020-01-01 0:00:00", 1577836800)]
+        [TestCase("1969-01-01 0:00:00", -31536000, Description = "With negative")]
+        public void ToDateTime_TestResults(string dtStr, int input)
+        {
+            DateTime expected = DateTime.Parse(dtStr);
+            DateTime actual = UnixTime.ToDateTime(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
