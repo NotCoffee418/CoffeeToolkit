23f6021cf4edd756648b3c6cfc388516e188ef39
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2021-07-25 02:11:21 +0200

    v1.0.4: Added Progress.PRogressTracker

---
+++ CoffeeToolkit.Tests/Progress/ProgressTrackerTests.cs
@@ -0,0 +1,58 @@
+﻿using CoffeeToolkit.Progress;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CoffeeToolkit.Tests.Progress
+{
+    class ProgressTrackerTests
+    {
+        [Test]
+        public void IncrementProgress_IncrementFirst_ValidateIncrementAndTimeEstimate()
+        {
+            // Increment pt 10 times with delay
+            ProgressTracker pt = new ProgressTracker(20);
+            for (int i = 0; i < 10; i++)
+            {
+                pt.IncrementProgress();
+                Thread.Sleep(100);
+            }
+
+            // Validate increment
+            Assert.AreEqual(10, pt.ProcessedItems);
+            Assert.AreNotEqual(DateTime.MinValue, pt.StartTime);
+
+            // Validate time estimate
+            var estMs = pt.GetEstimatedRemainingTime().TotalMilliseconds;
+            Assert.GreaterOrEqual(estMs, 1000);
+            Assert.LessOrEqual(estMs, 1500);
+        }
+
+        [Test]
+        public void ProgressEvent_IncrementTwo_ValidateEventsFired()
+        {
+            ProgressTracker pt = new ProgressTracker(2);
+            int eventFiredCount = 0;
+            pt.ProgressChanged += (s, e) =>
+            {
+                eventFiredCount++;
+                if (eventFiredCount == 1)
+                {
+                    Assert.AreEqual(1, e.ItemsProcessed);
+                }
+                else if (eventFiredCount == 2)
+                {
+                    Assert.AreEqual(0, e.TimeRemaining.TotalMilliseconds);
+                    Assert.AreEqual(100, e.ProgressPercentage);
+                }
+            };
+            pt.IncrementProgress();
+            pt.IncrementProgress();
+
+            Assert.AreEqual(2, eventFiredCount);
+        }
+    }
+}
---
+++ CoffeeToolkit/Progress/ProgressTracker.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeToolkit.Progress
+{
+    /// <summary>
+    /// Easy access to thread-safe progress tracking and completion estimator.
+    /// </summary>
+    public class ProgressTracker
+    {
+        public ProgressTracker(int totalItems)
+        {
+            _totalItems = totalItems;
+        }
+
+        /* Fields */
+        private int _processedItems = 0;
+        private int _totalItems = 0;
+
+        /* Events */
+        public delegate void ProgressChangedEventHandler(object sender, ProgressChangedEventArgs e);
+        public event ProgressChangedEventHandler ProgressChanged;
+
+
+        /* Properties */
+
+        /// <summary>
+        /// The amount of items that have been processed.
+        /// This should be incremented after each item or group of items.
+        /// </summary>
+        public int ProcessedItems
+        {
+            get { return _processedItems; }
+            set {
+                // Safety check for exceeding TotalItems
+                if (value > TotalItems)
+                    throw new ArgumentOutOfRangeException(
+                        "ProgressTracker reported that ProcessedItems was set to " +
+                        $"{value} while the maximum value is {TotalItems}.");
+
+                // Keep it positive
+                else if (value < 0)
+                    throw new ArgumentOutOfRangeException(
+                        "ProcessedItems must be a number between 0 and TotalItems");
+
+                // Automatically start tracking
+                if (_processedItems == 0 && StartTime == DateTime.MinValue)
+                    StartTime = DateTime.Now;
+
+                // Define field
+                _processedItems = value; 
+
+                // Fire event
+                
+                if (ProgressChanged != null)
+                    ProgressChanged(this, new ProgressChangedEventArgs() 
+                    { 
+                        ItemsProcessed = value,
+                        TotalItems = this.TotalItems,
+                        ProgressPercentage = GetProgressPercentage(),
+                        TimeRemaining = GetEstimatedRemainingTime(),
+                    });
+            }
+        }
+
+        /// <summary>
+        /// The amount of items that must be processed for the ProgressTracker to complete
+        /// </summary>
+        public int TotalItems
+        {
+            get { return _totalItems; }
+            set 
+            {
+                // Positive value check
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(
+                        "TotalItems must be greater than 0");
+
+                // TotalItems must always be greater than ProcessedItems
+                if (value < ProcessedItems)
+                    throw new ArgumentOutOfRangeException(
+                        "TotalItems must always be greater than ProcessedItems");
+
+                _totalItems = value; 
+            }
+        }
+
+        public DateTime StartTime { get; private set; } 
+            = DateTime.MinValue;
+
+        /// <summary>
+        /// Call this function to increase and recalculate progress.
+        /// </summary>
+        /// <param name="incrementAmount">Amount to increment</param>
+        public void IncrementProgress(int incrementAmount = 1)
+            => ProcessedItems++;
+
+        /// <summary>
+        /// Resets this ProgressTracker's progress count and start time.
+        /// This will be called automatically when adding your first item
+        /// </summary>
+        public void RestartTracker()
+        {
+            ProcessedItems = 0;
+            StartTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Gets a TimeSpan of the amount of time left before progress is complete.
+        /// Assumes that all items the same amount of time to process. Uses average.
+        /// </summary>
+        /// <returns></returns>
+        public TimeSpan GetEstimatedRemainingTime()
+        {
+            if (StartTime == DateTime.MinValue)
+                throw new InvalidOperationException(
+                    "You must call RestartTracker() before you can request a time estimate.");
+
+            // Calculate time needed
+            long elapsedMs = (long)Math.Round(DateTime.Now.Subtract(StartTime).TotalMilliseconds);
+            double millisecondsPerItem = elapsedMs / ProcessedItems;
+            int itemsRemaining = TotalItems - ProcessedItems;
+
+            var result = TimeSpan.FromMilliseconds(millisecondsPerItem * itemsRemaining);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the percentage of completed items
+        /// </summary>
+        /// <returns></returns>
+        public float GetProgressPercentage()
+            => (ProcessedItems / TotalItems) * 100;
+
+
+        public class ProgressChangedEventArgs
+        {
+            public int ItemsProcessed { get; internal set; }
+            public int TotalItems { get; internal set; }
+            public float ProgressPercentage { get; internal set; }
+            public TimeSpan TimeRemaining { get; internal set; }
+        }
+    }
+}
